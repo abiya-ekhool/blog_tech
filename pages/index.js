b3dac4aad6b3364e8caec8f59a0a51869b6f283d
edwin18g <edwin18g@gmail.com>
2024-07-08 18:45:22 +0000

    d

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,47 @@
 // pages/index.js
 import Head from 'next/head';
-import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const res = await fetch('/blogs.json');
+        const data = await res.json();
+        setBlogs(data);
+      } catch (error) {
+        console.error('Error fetching blog data:', error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>My Blog</title>
         <meta name="description" content="A blog created with Next.js and Bootstrap" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="container py-5">
-        <h1 className="text-center mb-4">Welcome to My change</h1>
+        <h1 className="text-center mb-4">Welcome to My Blog</h1>
 
         <div className="row">
           <div className="col-md-8">
-            <article className="mb-5">
-              <h2 className="h4">Blog Post Title</h2>
-              <p className="text-muted">Posted on January 1, 2024 by Author</p>
-              <p>This is a short excerpt from the blog post...</p>
-              <a href="#" className="btn btn-primary">Read more</a>
-            </article>
-            <article className="mb-5">
-              <h2 className="h4">Another Blog Post Title</h2>
-              <p className="text-muted">Posted on January 2, 2024 by Author</p>
-              <p>This is a short excerpt from the blog post...</p>
-              <a href="#" className="btn btn-primary">Read more</a>
-            </article>
-            {/* Add more blog posts here */}
+            {blogs.map((blog) => (
+              <article className="mb-5" key={blog.id}>
+                <h2 className="h4">{blog.title}</h2>
+                <p className="text-muted">Posted on {blog.date} by {blog.author}</p>
+                <p>{blog.excerpt}</p>
+                <a href={blog.link} className="btn btn-primary">Read more</a>
+              </article>
+            ))}
           </div>
           <div className="col-md-4">
             <aside>
@@ -49,9 +61,9 @@ export default function Home() {
               <div className="p-4">
                 <h4 className="font-italic">Elsewhere</h4>
                 <ol className="list-unstyled">
-                  <li><a href="#">GitHub</a></li>
-                  <li><a href="#">Twitter</a></li>
-                  <li><a href="#">Facebook</a></li>
+                  <li><a href="https://github.com">GitHub</a></li>
+                  <li><a href="https://twitter.com">Twitter</a></li>
+                  <li><a href="https://facebook.com">Facebook</a></li>
                 </ol>
               </div>
             </aside>
