1ce3748118f551bad5839c7e2a715268a52c4d45
Abiya <abiya.resbee@gmail.com>
2024-07-09 23:18:25 +0530

    domain name

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // pages/index.js
 import Head from 'next/head';
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 
@@ -30,43 +31,16 @@ export default function Home() {
       </Head>
 
       <main className="container py-5">
-        <h1 className="text-center mb-4">Welcome to Tech Plazaa</h1>
-
-        <div className="row">
+        <div className="text-center mb-4">
+          <img src="/w_logo.png" alt="Tech Plazaa Logo"  />
+        </div>
+        
+        <div className="row justify-content-center mb-4">
           <div className="col-md-8">
-            {blogs.map((blog) => (
-              <article className="mb-5" key={blog.id}>
-                <h2 className="h4">{blog.title}</h2>
-                <p className="text-muted">Posted on {blog.date} by {blog.author}</p>
-                <p>{blog.excerpt}</p>
-                <a href={blog.link} className="btn btn-primary">Read more</a>
-              </article>
-            ))}
-          </div>
-          <div className="col-md-4">
-            <aside>
-              <div className="p-4 mb-3 bg-light rounded">
-                <h4 className="font-italic">About</h4>
-                <p className="mb-0">This is a short description about the blog and its purpose.</p>
-              </div>
-              <div className="p-4">
-                <h4 className="font-italic">Archives</h4>
-                <ol className="list-unstyled mb-0">
-                  <li><a href="#">January 2024</a></li>
-                  <li><a href="#">December 2023</a></li>
-                  <li><a href="#">November 2023</a></li>
-                  {/* Add more archive links here */}
-                </ol>
-              </div>
-              <div className="p-4">
-                <h4 className="font-italic">Elsewhere</h4>
-                <ol className="list-unstyled">
-                  <li><a href="https://github.com">GitHub</a></li>
-                  <li><a href="https://twitter.com">Twitter</a></li>
-                  <li><a href="https://facebook.com">Facebook</a></li>
-                </ol>
-              </div>
-            </aside>
+            <div className="input-group input-group-lg">
+              <input type="search" className="form-control border-dark" placeholder="Innovate forward . . ." aria-label="Search" />
+              <button className="btn btn-dark" type="button">Search</button>
+            </div>
           </div>
         </div>
       </main>
