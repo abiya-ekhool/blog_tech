461ad5b01503f7f715cdc934e2be7a2bc4a82d78
edwin18g <edwin18g@gmail.com>
2024-07-10 16:53:43 +0000

    d

---
+++ pages/api/users.js
@@ -0,0 +1,19 @@
+// pages/api/users.js
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('Cluster0'); // Use the correct database name if different
+
+  switch (req.method) {
+    case 'POST':
+      const bodyObject = JSON.parse(req.body);
+      const newUser = await db.collection('users').insertOne(bodyObject);
+      res.json(newUser.ops[0]);
+      break;
+    case 'GET':
+      const users = await db.collection('users').find({}).toArray();
+      res.json({ status: 200, data: users });
+      break;
+  }
+}
