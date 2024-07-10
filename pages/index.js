dfd744e085f0bf38635c7154e14a96e24ba9e22a
edwin18g <edwin18g@gmail.com>
2024-07-10 16:08:03 +0000

    d

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // pages/index.js
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 
@@ -24,10 +25,34 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>techplazaa</title>
-        <meta name="description" content="A blog created with Next.js and Bootstrap" />
+        <title>Tech Plazaa - A Tech Blog</title>
+        <meta name="description" content="Explore the latest tech trends and innovations on Tech Plazaa, a blog created with Next.js and Bootstrap." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://www.techplazaa.com/" />
+        {/* Structured Data */}
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: `
+            {
+              "@context": "https://schema.org",
+              "@type": "Blog",
+              "publisher": {
+                "@type": "Organization",
+                "name": "Tech Plazaa",
+                "logo": {
+                  "@type": "ImageObject",
+                  "url": "https://www.techplazaa.com/logo.png",
+                  "width": 600,
+                  "height": 60
+                }
+              },
+              "mainEntityOfPage": {
+                "@type": "WebPage",
+                "@id": "https://www.techplazaa.com/"
+              }
+            }
+          `
+        }} />
       </Head>
 
       <main className="container py-5">
@@ -38,27 +63,31 @@ export default function Home() {
         <div className="row justify-content-center mb-4">
           <div className="col-md-8">
             <div className="input-group input-group-lg">
-              <input type="search" className="form-control border-dark" placeholder="Innovate forward . . ." aria-label="Search" />
+              <label htmlFor="searchInput" className="visually-hidden">Search</label>
+              <input id="searchInput" type="search" className="form-control border-dark" placeholder="Innovate forward . . ." aria-label="Search" />
               <button className="btn btn-dark" type="button">Search</button>
             </div>
           </div>
         </div>
 
-        <div className="row gy-2">
-          {blogs.map((blog, index) => (
-            <div key={index} className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay={`${(index + 1) * 100}`}>
-              <div className={`${styles.card} card`}>
-                <div className="icon">
-                  {/* <i className="bi bi-chat-square-text"></i> */}
+        <div className="row gy-4">
+        {blogs.map((blog) => (
+            <div key={blog.id} className="col-lg-4 col-md-6">
+              <div className={`card ${styles.blogCard} border-0 shadow-sm rounded-lg`} data-aos="fade-up">
+                <div className="card-body">
+                <Link href={`/blogs/${blog.slug}`} passHref legacyBehavior>
+  <a className="text-decoration-none">
+    <h4 className="card-title mb-3">{blog.title}</h4>
+  </a>
+</Link>
+
+                  <p className="card-text">{blog.excerpt}</p>
                 </div>
-                <a href="service-details.html" className="stretched-link">
-                  <h4>{blog.title}</h4>
-                </a>
-                <small>{blog.excerpt}</small>
               </div>
             </div>
           ))}
         </div>
+
       </main>
     </div>
   );
